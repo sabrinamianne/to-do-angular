4f48d6a746d1c3911ae2bcfab482e2611ee03712
Sabrina Mianne <sabrina.epicodus@gmail.com>
2019-06-10 11:11:49 -0700

    Create a to do list with angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Task} from './models/task.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Epicodus Angular Homework ';
+  currentFocus: string = 'To Do List';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth()+1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+tasks: Task[] = [
+  new Task('Finish weekend Angular homework for Epicodus course'),
+  new Task('Begin brainstorming possible JavaScript group projects'),
+  new Task('Add README file to last few Angular repos on GitHub')
+  ];
+}
