579358f8a613cf8a36b1478c3c9111f619eefcf5
Sabrina Mianne <sabrina.epicodus@gmail.com>
2019-06-18 15:46:41 -0700

    Add new components and functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,20 @@ import { Task} from './models/task.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Epicodus Angular Homework ';
   currentFocus: string = 'To Do List';
   currentTime = new Date();
   month: number = this.currentTime.getMonth()+1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
-  new Task('Finish weekend Angular homework for Epicodus course',1),
-  new Task('Begin brainstorming possible JavaScript group projects',2),
-  new Task('Add README file to last few Angular repos on GitHub',3)
+  selectedTask: Task = null;
+
+  masterTaskList: Task[] = [
+  new Task('Finish weekend Angular homework for Epicodus course', 3),
+  new Task('Begin brainstorming possible JavaScript group projects', 2),
+  new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-  priorityColor(currentTask) {
-    if (currentTask.priority ===3) {
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
-   selectedTask: Task = null;
 
    editTask(clickedTask) {
      this.selectedTask= clickedTask;
